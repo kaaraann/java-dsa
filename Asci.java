d1bffa4f293e060b8a55d9f981b2c7a3a0dafb87
kaaraann <karanthakur2k2k@gmail.com>
2025-07-22 19:19:11 +0530

    Asci Value

---
+++ Asci.java
@@ -0,0 +1,8 @@
+class Asci {
+   public static void main(String[] args) {
+
+      for (char ch = 'A'; ch <= 'z'; ch++) {
+         System.out.println(ch + " Ascii value is " + (int) ch);
+      }
+   }
+}
