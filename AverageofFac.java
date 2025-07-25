19a21cdaf8dae7285cd043ad24dc5cf86397cda0
kaaraann <karanthakur2k2k@gmail.com>
2025-07-25 09:29:22 +0530

    Average Of fac

---
+++ AverageofFac.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class AverageOfFac {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        int count = 0;
+
+        System.out.println("Factors of " + num + " are: ");
+        for(int i = 1; i <= num; i++) {
+            if(num % i == 0) {
+                System.out.print(i + " ");
+                sum = sum + i;
+                count++;
+            }
+        }
+
+        double average = (double)sum / count;
+        System.out.println("\nAverage of factors: " + average);
+    }
+}
